e89069b0022a2937b967d36b6a4f243d7356b0e7
Agustin Casal <agustincasal@isft220.edu.ar>
2025-05-30 08:16:41 -0300

    Clase 29 de Mayo

---
+++ Clase 05-29/src/index.ts
@@ -0,0 +1,7 @@
+function saludar(nombre: string):string {
+    return `Hola, ${nombre}!`;
+}
+
+const usuario = "Agustin";
+
+console.log(saludar(usuario));
--- entorno.md
+++
@@ -1,3 +0,0 @@
-Utilizo Windows 10 23H2.
-Git version 2.49.0.windows.1
-Elijo utilizar Visual Studio Code
