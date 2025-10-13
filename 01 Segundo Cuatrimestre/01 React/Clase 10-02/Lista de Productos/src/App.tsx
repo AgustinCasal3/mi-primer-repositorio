fae76635c3c5e01c1a0578292de999e4192f16c3
AgustinCasal3 <agustincasal@isft220.edu.ar>
2025-10-13 16:32:22 -0300

    Clase 10-09

--- 01 Segundo Cuatrimestre/01 React/Clase 10-02/Lista de Productos/src/App.tsx
+++ 01 Segundo Cuatrimestre/01 React/Clase 10-02/Lista de Productos/src/App.tsx
@@ -1,23 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { TarjetaProducto } from './Producto'
+import { Reloj } from './Reloj'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="contenido">
         <h1>Nuestros Productos</h1>
+      <div className="contenido">
 
         {TarjetaProducto({nombre: "Mococo Friends with U", precio: 40, stock: true, imagen: '../public/imgs/Mococo.png'})}
-        <br />
         {TarjetaProducto({nombre: "Fuwawa Friends with U", precio: 45, stock: false, imagen: '../public/imgs/Fuwawa.png'})}
-        <br />
         {TarjetaProducto({nombre: "Aqua Friends with U", precio: 35, stock: true, imagen: '../public/imgs/Aqua.png'})}
       </div>
+        {Reloj()}
     </>
   )
 }
--- 01 Segundo Cuatrimestre/01 React/Clase 10-02/Lista de Productos/src/Producto.tsx
+++ 01 Segundo Cuatrimestre/01 React/Clase 10-02/Lista de Productos/src/Producto.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './Producto.css'
 
 interface Producto {
@@ -12,18 +13,28 @@ export function TarjetaProducto({nombre, precio, stock, imagen}: Producto) {
     const claseStock = stock ? 'disponible' : 'agotado';
     const textoStock = stock ? 'Hay Stock' : 'No hay Stock';
 
+    const [count, setCount] = useState(0);
+    const incrementar = () => {setCount(count + 1)};
+    const decrementar = () => {setCount(count - 1)};
 
     return (
-        <div className="contenedor">
-            <div className="img">
-                <img src={imagen} alt="Imagen del producto"/>
-            </div>
-            <div className="texto">
-                <h3>{nombre}</h3>
-                <h2>${precio}</h2>
-            </div>
-            <div className={claseStock}>
-                <h3>{textoStock}</h3>
+        <div className="producto">
+            <div className="contenedor">
+                <div className="img">
+                    <img src={imagen} alt="Imagen del producto"/>
+                </div>
+                <div className="texto">
+                    <h3>{nombre}</h3>
+                    <h2>${precio}</h2>
+                </div>
+                <div className="divContador">
+                    <button onClick={incrementar}>+</button>
+                    <p>{count}</p>
+                    <button onClick={decrementar}>-</button>
+                </div>
+                <div className={claseStock}>
+                    <h3>{textoStock}</h3>
+                </div>
             </div>
         </div>
     )
