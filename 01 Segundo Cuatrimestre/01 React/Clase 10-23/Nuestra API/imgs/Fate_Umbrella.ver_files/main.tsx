4b36ae4fb02fce008494a309c2a60b56a279709c
Agustin Casal <agustincasal@isft220.edu.ar>
2025-12-29 22:53:09 -0300

    Ultima modificacion

---
+++ 01 Segundo Cuatrimestre/01 React/Clase 10-23/Nuestra API/imgs/Fate_Umbrella.ver_files/main.tsx
@@ -0,0 +1,18 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=0695bed1"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=0695bed1"; const StrictMode = __vite__cjsImport1_react["StrictMode"];
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=0695bed1"; const createRoot = __vite__cjsImport2_reactDom_client["createRoot"];
+import "/src/index.css";
+import App from "/src/App.tsx?t=1765225851921";
+createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+    fileName: "D:/Escritorio/Fate-Umbrella.ver/0 Fate Umbrella.ver Web/Fate Umbrella.ver/src/main.tsx",
+    lineNumber: 9,
+    columnNumber: 5
+  }, this) }, void 0, false, {
+    fileName: "D:/Escritorio/Fate-Umbrella.ver/0 Fate Umbrella.ver Web/Fate Umbrella.ver/src/main.tsx",
+    lineNumber: 8,
+    columnNumber: 3
+  }, this)
+);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBUUk7QUFSSixTQUFTQSxrQkFBa0I7QUFDM0IsU0FBU0Msa0JBQWtCO0FBQzNCLE9BQU87QUFDUCxPQUFPQyxTQUFTO0FBR2hCRCxXQUFXRSxTQUFTQyxlQUFlLE1BQU0sQ0FBRSxFQUFFQztBQUFBQSxFQUMzQyx1QkFBQyxjQUNDLGlDQUFDLFNBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQSxTQUFJLEtBRE47QUFBQTtBQUFBO0FBQUE7QUFBQSxTQUVBO0FBQ0YiLCJuYW1lcyI6WyJTdHJpY3RNb2RlIiwiY3JlYXRlUm9vdCIsIkFwcCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZXMiOlsibWFpbi50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgU3RyaWN0TW9kZSB9IGZyb20gJ3JlYWN0J1xyXG5pbXBvcnQgeyBjcmVhdGVSb290IH0gZnJvbSAncmVhY3QtZG9tL2NsaWVudCdcclxuaW1wb3J0ICcuL2luZGV4LmNzcydcclxuaW1wb3J0IEFwcCBmcm9tICcuL0FwcC50c3gnXHJcblxyXG5cclxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpISkucmVuZGVyKFxyXG4gIDxTdHJpY3RNb2RlPlxyXG4gICAgPEFwcCAvPlxyXG4gIDwvU3RyaWN0TW9kZT4sXHJcbikiXSwiZmlsZSI6IkQ6L0VzY3JpdG9yaW8vRmF0ZS1VbWJyZWxsYS52ZXIvMCBGYXRlIFVtYnJlbGxhLnZlciBXZWIvRmF0ZSBVbWJyZWxsYS52ZXIvc3JjL21haW4udHN4In0=
